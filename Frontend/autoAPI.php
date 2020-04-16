3dab30b2f28e10a89aed2c3b3d07465089141911
Siddhartha Mishra <siddhartham04@gmail.com>
2020-04-16 14:29:21 -0700

    More work on API building

--- Frontend/autoAPI.php
+++ Frontend/autoAPI.php
@@ -1,25 +1,27 @@
 <?php
-
+header("Content-Type: application/json"); 
 $name = $_GET["loc"]; 
-echo $name; 
+echo "hellp"; 
 $url = "https://maps.googleapis.com/maps/api/place/autocomplete/json?"; 
-$data = array("input" => "Gurg", "types" => "address", "key" => "AIzaSyDmpN-plRNCFrhIRCBNKjvfGuRASDK4EOg"); 
+$data = array("input" => "Gurg", "types" => "address", "key" => "AIzaSyCZRvqG4MqNFZRQH62bwcHuANO_1MqHCio"); 
 
 $options = array(
-    'http' => array(
-        'method' => "POST", 
-        'content' => http_build_query($data)
-    ),
-    "ssl" => [
+    // 'http' => array(
+    //     'method' => "POST", 
+    //     'content' => http_build_query($data)
+    // ),
+    "ssl" => array(
         "verify_peer"=>false,
         "verify_peer_name"=>false,
-    ]
+    )
 ); 
 
 $context = stream_context_create($options); 
 
-$result = file_get_contents($url, false, $context); 
+//$result = file_get_contents($url, false, $)
+$result = file_get_contents($url.http_build_query($data), false, $context); 
 echo $result; 
+
 
 
 ?>
