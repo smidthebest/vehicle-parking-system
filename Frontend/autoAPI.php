147cf4374fff2259b052ad8efd25218a1a7525d9
Siddhartha Mishra <siddhartham04@gmail.com>
2020-04-16 17:32:17 -0700

    Autocomplete Works!

--- Frontend/autoAPI.php
+++ Frontend/autoAPI.php
@@ -1,9 +1,15 @@
 <?php
 header("Content-Type: application/json"); 
-$name = $_GET["loc"]; 
-echo "hellp"; 
+$name = $_GET["term"]; 
+$lat = $_GET["lat"];
+$long = $_GET["long"]; 
 $url = "https://maps.googleapis.com/maps/api/place/autocomplete/json?"; 
-$data = array("input" => "Gurg", "types" => "address", "key" => "AIzaSyCZRvqG4MqNFZRQH62bwcHuANO_1MqHCio"); 
+$data = array(
+    "input" => $name, 
+    "types" => "address",
+     "location" => "$lat,$long",
+     "radius" => 500, 
+    "key" => "AIzaSyCZRvqG4MqNFZRQH62bwcHuANO_1MqHCio"); 
 
 $options = array(
     // 'http' => array(
@@ -20,8 +26,14 @@ $context = stream_context_create($options);
 
 //$result = file_get_contents($url, false, $)
 $result = file_get_contents($url.http_build_query($data), false, $context); 
-echo $result; 
 
+$array_names = json_decode($result, true); 
+$ans = array(); 
+foreach($array_names["predictions"] as $key => $value){
+    array_push($ans, $value["description"]); 
+}
+
+echo json_encode($ans); 
 
 
 ?>
--- Frontend/index.php
+++ Frontend/index.php
@@ -1,3 +1,5 @@
+
+
 <!DOCTYPE html>
 <html>
     <head>
@@ -9,44 +11,32 @@
             <script src="https://unpkg.com/leaflet@1.6.0/dist/leaflet.js"
             integrity="sha512-gZwIG9x3wUXg2hdXF6+rVkLF/0Vi9U8D2Ntg4Ga5I5BZpVkVxlJWbSQtXPSiUTtC0TjtGOmxa1AJPuV0CPthew=="
             crossorigin=""></script>
-            <script src="//code.jquery.com/jquery-1.12.4.js"></script>
-            <script src="//code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+           <script src="//code.jquery.com/jquery-1.12.4.js"></script>
+            <script src="//code.jquery.com/ui/1.12.1/jquery-ui.js"></script> 
+           <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script> -->
 
     </head>
     <body>
-        <div style="height: 100px">
-            <form action = "/index.php" method="POST">
+        <div style="height: 300px">
+            <form >
                 <label for="auto">Put Address here: </label>
                 
-                <input id="auto" name="loc" style=" width: 200px" type="text">
-                <button type="submit" name="submit" >Submit</button>
+                <input  name = "term" id="auto"style=" width: 200px" type="text">
+                <button type="submit"  >Submit</button>
             </form>
                 
         </div>
         <div id="mapid" style="height: 500px; width: 500px; float: left;"></div>
         
     </body>
-    <?php
-        if(isset($_POST['submit']))
-        {
-            $name = $_POST['loc'];
-            
-            $url = "http://localhost:8081/autoAPI.php".$name;
-            
-            $client = curl_init($url);
-            curl_setopt($client,CURLOPT_RETURNTRANSFER,true);
-            $response = curl_exec($client);
-            
-            $result = json_decode($response);
-            
-            echo $result->data; 
-        }
-    ?>
+   
     <script>
-        $("#auto").autocomplete({
-            source: ["hello", "how are you", "why", "apple"]
-        })
+       
+        
+       
          var mymap = L.map('mapid').setView([28.6139, 77.209], 8);
+         var lat = mymap.getCenter().lat; 
+        var long = mymap.getCenter().lng; 
         L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
             attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
             maxZoom: 18,
@@ -55,6 +45,13 @@
             zoomOffset: -1,
             accessToken: 'pk.eyJ1Ijoic21pZGRtaXNoIiwiYSI6ImNrOTA4a2xwajAwNWozZXM1c2FnanlmNTQifQ.f53gGVXjiBqVqYXum8n8wA'
         }).addTo(mymap);
+        
+       
+        $("#auto").autocomplete({  
+            
+            source: "/autoAPI.php?lat="+lat+"&long="+long
+            
+        })
 
     </script>
 </html>
