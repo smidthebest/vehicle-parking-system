4867cfcdedef64710614d5c8aed49f80ba6380cb
Siddhartha Mishra <siddhartham04@gmail.com>
2020-04-17 15:50:37 -0700

    comments

--- Frontend/autoAPI.php
+++ Frontend/autoAPI.php
@@ -2,10 +2,11 @@
 require "util.php"; 
 
 header("Content-Type: application/json"); 
-$name = $_GET["term"]; 
-$lat = $_GET["lat"];
-$long = $_GET["long"]; 
+$name = $_GET["term"]; //what needs to be autocompleted by API
+$lat = $_GET["lat"]; //latitude of the center of map for biasing results
+$long = $_GET["long"]; //longitude of the center of map for biasing results 
 $url = "https://maps.googleapis.com/maps/api/place/autocomplete/json?"; 
+
 $data = array(
     "input" => $name, 
     "types" => "address",
@@ -14,8 +15,11 @@ $data = array(
     "key" => "AIzaSyCZRvqG4MqNFZRQH62bwcHuANO_1MqHCio"
 ); 
 
+//gets json data from places API 
 $result = get_from_link($url.http_build_query($data)); 
 
+//decodes json to filter out irrelevatn information and return the location 
+// addresses. 
 $array_names = json_decode($result, true); 
 $ans = array(); 
 foreach($array_names["predictions"] as $key => $value){
