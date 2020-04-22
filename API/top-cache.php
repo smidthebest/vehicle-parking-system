ee80fe1e02ce78d142c975b5f6c457a6f5d7c202
Siddhartha Mishra <siddhartham04@gmail.com>
2020-04-21 17:11:33 -0700

    refined cacheing accuracy

--- API/top-cache.php
+++ API/top-cache.php
@@ -1,12 +1,12 @@
 <?php
 $url = $_SERVER["REQUEST_URI"];
 $break = Explode('?', $url);
-$file = $break[1];
-$cachefile = 'cache/cached-'.$file.'.html';
-$cachetime = 18000;
+$file = Explode('&', $break[1]);
+$ans = Explode('.', $file[0])[0]."-".Explode('.', $file[1])[0].$file[2]; 
+$cachefile = 'cache/cached-'.$ans.'.json';
+$cachetime = 2.592*pow(10,6);
 // Serve from the cache if it is younger than $cachetime
 if (file_exists($cachefile) && time() - $cachetime < filemtime($cachefile)) {
-   
     readfile($cachefile);
     exit;
 }
