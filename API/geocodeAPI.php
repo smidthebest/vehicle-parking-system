115c2629e52ff46bbb2656ed0e641ac5a19ed123
Siddhartha Mishra <siddhartham04@gmail.com>
2020-04-21 17:16:33 -0700

    implemented cache for geocode

--- API/geocodeAPI.php
+++ API/geocodeAPI.php
@@ -1,5 +1,6 @@
 <?php
 require "util.php"; 
+include("top-cache.php"); 
 $loc = $_GET['address']; 
 header("Content-Type: application/json"); 
 $url = "https://maps.googleapis.com/maps/api/geocode/json?";
@@ -14,6 +15,6 @@ $result = get_from_link($url.http_build_query($data));
 
  $array = json_decode($result, true); 
  echo json_encode( $array["results"][0]["geometry"]["location"]); 
-
+include("bottom-cache.php"); 
 ?>
 
