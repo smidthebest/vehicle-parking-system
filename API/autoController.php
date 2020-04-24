acd9272d540c267d5727a7ba28411ecf4c077645
Siddhartha Mishra <siddhartham04@gmail.com>
2020-04-24 15:15:28 -0700

    sending headers in autocomplete

--- API/autoController.php
+++ API/autoController.php
@@ -4,9 +4,7 @@ require_once "cache.php";
 
 $cache = top_cache(); 
 
-header("Access-Control-Allow-Origin: *");
-header("Content-Type: application/json; charset=UTF-8");
-header("Access-Control-Allow-Methods: GET");
+header("Content-Type: application/json;");
 
 $requestMethod = $_SERVER["REQUEST_METHOD"];
 
